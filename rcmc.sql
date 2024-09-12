fde99c64e98885ee37b1673e0e8b8b7f36d728c5
benjamin martinez <benjamartinez454gmail.com>
2024-09-12 12:02:52 -0300

    Avanzamos con la creacion de las tablas

---
+++ rcmc.sql
@@ -0,0 +1,48 @@
+use minecraft;
+show tables;
+
+CREATE TABLE items (
+  ID INT PRIMARY KEY,
+  nombre_item VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE jugadores (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  nombres VARCHAR(20),
+  inventario_id INT FOREIGN KEY,
+  ubicacionuser_id INT FOREIGN KEY ,
+  fecha_registro DATE,
+);
+
+
+CREATE TABLE inventario (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  items_id VARCHAR,
+  inventario_id INT PRIMARY KEY,
+  ubicacionuser_id INT PRIMARY KEY,
+  fecha_registro DATE,
+);
+
+
+CREATE TABLE mobs (
+  ID INT PRIMARY KEY,
+  nombre_mob VARCHAR,
+  tipo_mob TEXT,
+  dano_mob INT BOOLEAN,
+  movilidad INT BOOLEAN,
+  distancia_ataque INT,
+  bioma INT FOREIGN KEY,
+  interaccion_id INT FOREIGN KEY,
+  reproduccion BOOLEAN,
+);
+
+
+CREATE TABLE interaccionuser (
+  ID INT,
+  user_id INT FOREIGN KEY,
+  mob_id INT FOREIGN KEY,
+  tipo_interaccion TEXT,
+  fecha_interaccion DATE,
+);
+
