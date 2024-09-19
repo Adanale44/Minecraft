536cf634f35d3d591e494914d04108d4c2d4ef38
KevinOrosco <kevinorosco411@gmail.com>
2024-09-19 12:10:04 -0300

    datos de la tabla mobs

--- minels/lsts.sql
+++ minels/lsts.sql
@@ -1,2 +1,6 @@
-INSERT INTO mobs(nombre_mob, tipo_mob, dano_mob,vida_mob, movilidad, distancia_ataque, bioma, interaccion_id, reproduccion);
-VALUES ('Zombi', 'Hostil', '3', '20', 'terrestre', 'cuerpo a cuerpo', '', 'atacar', 'si' );
+INSERT INTO mobs(nombre_mob, tipo_mob, dano_mob, vida_mob, movilidad, distancia_ataque, bioma, interaccion_id, reproduccion);
+VALUES ('Zombi', 'Hostil', '3', '20', 'Terrestre', 'Cuerpo a cuerpo', '', 'Atacar', 'no' ),
+('Esqueleto', 'Hostil', '3', '20', 'Terrestre', 'A distancia', '', 'Atacar', 'no'),
+('Creeper', 'Hostil', '42', '20', 'Terrestre', 'Cuerpo a cuerpo','', 'Atacar', 'no'),
+('Araña', 'Neutral', '2', '16', 'Terrestre', 'Cuerpo a cuerpo', '', 'Atacar', 'no'),
+('Enderman', 'Neutral', '6', '40', 'Terrestre', 'Cuerpo a cuerpo', '', 'Atacar', 'no'); 
