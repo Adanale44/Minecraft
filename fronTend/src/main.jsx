833b53f19b05d3b480da81d62d27828fa3ef31e6
adna <dbsyc1214@gmail.com>
2024-10-23 17:09:19 -0300

    hice mas cosas para la pagina web

--- fronTend/src/main.jsx
+++ fronTend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
+import {createBrowerRouter, RouterProvider} from "react-router-dom";
 
+const router = createBrowerRouter([
+  {
+    path: "/",
+    element: <App/>,
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router }/>
   </StrictMode>,
 )
