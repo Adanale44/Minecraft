c5788f541781236d3206259c2474ea7581d2934f
benjamin martinez <benjamartinez454gmail.com>
2024-10-31 12:07:58 -0300

    añadi las tablas de jefes y daño de jefes, todavia no las inserte

---
+++ jefes/daño_jefe.sql
@@ -0,0 +1,15 @@
+INSERT INTO caracteristicas_jefe (efectos, comportamiento,
+  , apariencia, ataque_a_distancia, ataque_mele, habilidad_especial
+) VALUES
+(
+    NULL, "Hostil", "Altura:8 Bloques Anchura:16 Bloques Longitud:16 Bloques", 
+    "Aliento de dragón", "Daño cuerpo a cuerpo", "Bola de fuego de dragón"
+),
+(
+    "Descomposición", "Hostil", "Altura: 3.5 Bloques Ancho: 0.9 Bloques", 
+    "Calavera del Wither", "Ataque de impulso", "Explosión al invocarse"
+),
+(
+   "Descomposición", "Hostil", "Altura:1.9975 Bloques Anchura:1.9975 Bloques", 
+    "Laser", "Spikes", "Explosión al invocarse"
+);
